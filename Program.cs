841df6f6b1b0acdd9a2a92361e814db7e8bba5bb
Tushar Verma <tushar.b.verma@accenture.com>
2021-06-06 09:41:34 +0530

    Intilize DotNet Console Application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Songs_Recommendation_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ELK Stack is Live at https://songs-recommendation-engine.es.asia-south1.gcp.elastic-cloud.com:9243/");
+        }
+    }
+}
